9f7052294529d04115769740912eb620d6011e88
jiyinyiyong <jiyinyiyong@gmail.com>
2017-09-07 17:05:47 +0800

    read extension from cli; release 0.2.5 on npm

--- server/src/server/util/env.cljs
+++ server/src/server/util/env.cljs
@@ -5,8 +5,10 @@
   (let [cli-port (if (some? (aget js/process.env "port"))
                    (js/parseInt (aget js/process.env "port")))
         cli-op (aget js/process.env "op")
+        cli-extension (aget js/process.env "extension")
         result (-> configs
                    (update :port (fn [port] (or cli-port port)))
-                   (update :op (fn [op] (or cli-op op))))]
+                   (update :op (fn [op] (or cli-op op)))
+                   (update :extension (fn [extension] (or cli-extension extension))))]
     (println "Using configs:" result)
     result))
