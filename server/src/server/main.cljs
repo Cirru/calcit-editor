7c4ca2bb57ebe9a24004899c1ec44cf859ed2d79
jiyinyiyong <jiyinyiyong@gmail.com>
2017-11-22 02:06:31 +0800

    updated editor server

--- server/src/server/main.cljs
+++ server/src/server/main.cljs
@@ -1,7 +1,7 @@
 
 (ns server.main
   (:require [server.schema :as schema]
-            [server.network :refer [run-server! render-clients!]]
+            [server.network :refer [run-server! sync-clients!]]
             [server.updater.core :refer [updater]]
             [cljs.core.async :refer [<! >!]]
             [cljs.reader :refer [read-string]]
@@ -24,36 +24,32 @@
          (assoc :saved-files (get-in db [:ir :files]))
          (update :configs (fn [configs] (or configs schema/configs)))))))
 
+(defn dispatch! [op op-data sid op-id op-time]
+  (comment .log js/console "Action" (str op) (clj->js op-data) sid op-id op-time)
+  (comment .log js/console "Database:" (clj->js @*writer-db))
+  (cond
+    (= op :effect/save-files) (handle-files! @*writer-db schema/configs dispatch! true)
+    :else
+      (try
+       (let [new-db (updater @*writer-db op op-data sid op-id op-time)]
+         (reset! *writer-db new-db))
+       (catch js/Error e (println (.red chalk e))))))
+
 (defonce *reader-db (atom @*writer-db))
+
+(defn proxy-dispatch! [& args] (apply dispatch! args))
 
 (defn render-loop! []
   (if (not= @*reader-db @*writer-db)
     (do
      (reset! *reader-db @*writer-db)
      (comment println "render loop")
-     (render-clients! @*reader-db)))
+     (sync-clients! @*reader-db)))
   (js/setTimeout render-loop! 20))
 
 (defn start-server! [configs]
-  (let [server-ch (run-server! {:port (:port configs)})]
-    (go-loop
-     []
-     (let [[op op-data session-id op-id op-time] (<! server-ch)
-           dispatch! (fn [op' op-data']
-                       (go (>! server-ch [op' op-data' session-id op-id op-time])))]
-       (comment .log js/console "Action" (str op) (clj->js op-data) session-id op-id op-time)
-       (comment .log js/console "Database:" (clj->js @*writer-db))
-       (try
-        (do
-         (cond
-           (= op :effect/save-files) (handle-files! @*writer-db configs dispatch! true)
-           :else
-             (let [new-db (updater @*writer-db op op-data session-id op-id op-time)]
-               (reset! *writer-db new-db))))
-        (catch js/Error e (println (.red chalk e))))
-       (recur)))
-    (render-loop!))
-  (add-watch *reader-db :log (fn [] ))
+  (run-server! proxy-dispatch! (:port configs))
+  (render-loop!)
   (.on
    js/process
    "SIGINT"
@@ -62,13 +58,6 @@
      (println (str "\n" "Saved coir.edn") (str (if (some? code) (str "with " code))))
      (.exit js/process))))
 
-(defn compile-all-files! [configs]
-  (handle-files!
-   (assoc @*writer-db :saved-files {})
-   configs
-   (fn [op op-data] (println "After compile:" op op-data))
-   false))
-
 (defn serve-app! [port]
   (let [app (express), dir (path/join js/__dirname "app"), file-port (+ 100 port)]
     (.use app "/" (.static express dir) (serve-index dir (clj->js {:icons true})))
@@ -76,10 +65,17 @@
     (println
      (str "Serving local editor at " (.blue chalk (str "http://localhost:" file-port))))))
 
+(defn compile-all-files! [configs]
+  (handle-files!
+   (assoc @*writer-db :saved-files {})
+   configs
+   (fn [op op-data] (println "After compile:" op op-data))
+   false))
+
 (defn main! []
   (let [configs (pick-configs (:configs @*writer-db)), op (get configs :op)]
     (if (= op "compile")
       (compile-all-files! configs)
       (do (start-server! configs) (serve-app! (:port configs))))))
 
-(defn reload! [] (println (.gray chalk "code updated.")) (render-clients! @*reader-db))
+(defn reload! [] (println (.gray chalk "code updated.")) (sync-clients! @*reader-db))
--- server/src/server/network.cljs
+++ server/src/server/network.cljs
@@ -2,85 +2,76 @@
 (ns server.network
   (:require [cljs.nodejs :as nodejs]
             [cljs.reader :as reader]
-            [cljs.core.async :refer [chan >!]]
             [server.twig.container :refer [twig-container]]
             [recollect.diff :refer [diff-twig]]
             [recollect.twig :refer [render-twig]]
+            ["shortid" :as shortid]
             ["chalk" :as chalk]
-            [server.util.detect :refer [port-taken?]])
-  (:require-macros [cljs.core.async.macros :refer [go]]))
+            [server.util.detect :refer [port-taken?]]
+            ["uws" :as ws]))
 
-(defonce socket-registry (atom {}))
+(defonce *registry (atom {}))
 
-(defonce server-chan (chan))
+(defn error-port-taken! [port]
+  (do
+   (println
+    (.red
+     chalk
+     (str
+      "Failed to start server, port "
+      port
+      " is in use!\nYou can try `port="
+      (inc port)
+      " cumulo-editor`.")))
+   (.exit js/process 1)))
 
-(def shortid (js/require "shortid"))
-
-(def ws (js/require "uws"))
-
-(def WebSocketServer (.-Server ws))
-
-(defn handle-message [op op-data session-id]
-  (let [op-id (.generate shortid), op-time (.valueOf (js/Date.))]
-    (go (>! server-chan [op op-data session-id op-id op-time]))))
-
-(defn run-server! [configs]
-  (let [port (:port configs)]
+(defn run-server! [on-action! port]
+  (let [WebSocketServer (.-Server ws)]
     (port-taken?
      port
      (fn [err taken?]
        (if (some? err)
          (do (.error js/console err) (.exit js/process 1))
          (if taken?
-           (do
-            (println
-             (.red
-              chalk
-              (str
-               "Failed to start server, port "
-               port
-               " is in use!\nYou can try `port="
-               (inc port)
-               " cumulo-editor`.")))
-            (.exit js/process 1))
+           (error-port-taken! port)
            (let [wss (new WebSocketServer (js-obj "port" port))]
              (.on
               wss
               "connection"
               (fn [socket]
-                (let [session-id (.generate shortid)]
-                  (handle-message :session/connect nil session-id)
-                  (swap! socket-registry assoc session-id socket)
-                  (println (.gray chalk (str "client connected: " session-id)))
+                (let [sid (.generate shortid)
+                      op-id (.generate shortid)
+                      op-time (.valueOf (js/Date.))]
+                  (on-action! :session/connect nil sid op-id op-time)
+                  (swap! *registry assoc sid socket)
+                  (println (.gray chalk (str "client connected: " sid)))
                   (.on
                    socket
                    "message"
                    (fn [rawData]
                      (let [action (reader/read-string rawData), [op op-data] action]
-                       (handle-message op op-data session-id))))
+                       (on-action! op op-data sid op-id op-time))))
                   (.on
                    socket
                    "close"
                    (fn []
-                     (println (.gray chalk (str "client disconnected: " session-id)))
-                     (swap! socket-registry dissoc session-id)
-                     (handle-message :session/disconnect nil session-id))))))
+                     (println (.gray chalk (str "client disconnected: " sid)))
+                     (swap! *registry dissoc sid)
+                     (on-action! :session/disconnect nil sid op-id op-time))))))
              (println
               "Server started, please edit on"
-              (.blue chalk (str "http://cumulo-editor.cirru.org?port=" port)))))))))
-  server-chan)
-
-(def diff-options {:key :id})
+              (.blue chalk (str "http://cumulo-editor.cirru.org?port=" port))))))))))
 
 (defonce client-caches (atom {}))
 
-(defn render-clients! [db]
-  (doseq [session-entry (:sessions db)]
-    (let [[session-id session] session-entry
+(defn sync-clients! [db]
+  (doseq [sid (keys @*registry)]
+    (let [session-id sid
+          session (get-in db [:sessions sid])
           old-store (or (get @client-caches session-id) nil)
           new-store (render-twig (twig-container db session) old-store)
-          changes (diff-twig old-store new-store diff-options)
-          socket (get @socket-registry session-id)]
+          changes (diff-twig old-store new-store {:key :id})
+          socket (get @*registry session-id)]
       (comment .info js/console "Changes for" session-id ":" (clj->js changes))
       (if (and (not (empty? changes)) (some? socket))
         (do (.send socket (pr-str changes)) (swap! client-caches assoc session-id new-store))))))
