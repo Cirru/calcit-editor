a614fd807e92158f31f83354c7e9e9803fbc4e56
jiyinyiyong <jiyinyiyong@gmail.com>
2018-09-02 03:54:07 +0800

    use clipboard API

--- src/app/comp/expr.cljs
+++ src/app/comp/expr.cljs
@@ -11,7 +11,6 @@
             [app.client-util :refer [coord-contains? simple? leaf? expr?]]
             [app.util.shortcuts :refer [on-window-keydown on-paste!]]
             [app.theme :refer [decide-expr-theme]]
-            ["copy-text-to-clipboard" :as copy!]
             [app.util :refer [tree->cirru]]))
 
 (defn on-focus [coord] (fn [e d! m!] (d! :writer/focus coord)))
@@ -39,7 +38,15 @@
         (= code keycode/left) (do (d! :writer/go-left nil) (.preventDefault event))
         (= code keycode/right) (do (d! :writer/go-right nil) (.preventDefault event))
         (and meta? (= code keycode/c))
-          (do (copy! (pr-str (tree->cirru expr))) (d! :notify/push-message [:info "Copied!"]))
+          (do
+           (-> js/navigator
+               .-clipboard
+               (.writeText (pr-str (tree->cirru expr)))
+               (.then (fn [] (d! :notify/push-message [:info "Copied!"])))
+               (.catch
+                (fn [error]
+                  (.error js/console "Failed to copy:" error)
+                  (d! :notify/push-message [:error "Failed to copy!"])))))
         (and meta? (= code keycode/x)) (do (d! :ir/delete-node nil))
         (and meta? (= code keycode/v)) (on-paste! d!)
         (and meta? (= code keycode/b)) (d! :ir/duplicate nil)
