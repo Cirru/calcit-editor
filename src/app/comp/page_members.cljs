4167c04312000942888dd37971ee1a9faa712a86
jiyinyiyong <jiyinyiyong@gmail.com>
2018-09-05 00:10:51 +0800

    fix watching url

--- src/app/comp/page_members.cljs
+++ src/app/comp/page_members.cljs
@@ -60,7 +60,7 @@
                (=< 32 nil)
                (if (= k session-id)
                  (a
-                  {:href (str (.-href js/location) "&watching=" k),
+                  {:href (str (.-href js/location) "?watching=" k),
                    :target "_blank",
                    :style {:color (hsl 240 80 80)}}
                   (<> span "Watching url" nil))))])))))))
