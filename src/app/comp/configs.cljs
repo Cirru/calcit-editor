d732ad662c52bae5e38bb730cad02ddcab635a1c
jiyinyiyong <jiyinyiyong@gmail.com>
2020-12-16 15:19:44 +0800

    support editing modules field in configs

--- src/app/comp/configs.cljs
+++ src/app/comp/configs.cljs
@@ -8,6 +8,8 @@
             [cirru-edn.core :as cirru-edn]
             [respo-alerts.core :refer [use-prompt]]))
 
+(def style-value {:cursor :pointer, :font-family ui/font-code, :color (hsl 200 90 80)})
+
 (defcomp
  comp-configs
  (states configs)
@@ -16,21 +18,40 @@
                        {:text "Set a version:",
                         :initial (:version configs),
                         :placeholder "a version number...",
+                        :input-style {:font-family ui/font-code}})
+       modules-plugin (use-prompt
+                       (>> states :modules)
+                       {:text "Add modules:",
+                        :initial (string/join " " (:modules configs)),
+                        :placeholder "module/compact.cirru etc.",
                         :input-style {:font-family ui/font-code}})]
    (div
     {:style (merge ui/expand ui/column {:padding "0 16px"})}
     (=< nil 8)
     (div
-     {:style (merge ui/expand)}
+     {}
      (<> "Version:" {:font-family ui/font-fancy})
      (=< 8 nil)
      (span
       {:on-click (fn [e d!]
          ((:show version-plugin) d! (fn [text] (d! :configs/update {:version text}))))}
+      (<> (if (string/blank? (:version configs)) "-" (:version configs)) style-value)))
+    (div
+     {}
+     (<> "Modules:" {:font-family ui/font-fancy})
+     (=< 8 nil)
+     (span
+      {:on-click (fn [e d!]
+         ((:show modules-plugin)
+          d!
+          (fn [text]
+            (d! :configs/update {:modules (remove string/blank? (string/split text " "))}))))}
       (<>
-       (if (string/blank? (:version configs)) "-" (:version configs))
-       {:cursor :pointer, :font-family ui/font-code, :color (hsl 200 90 80)}))
-     (pre
-      {:style (merge {:max-width "100%", :overflow :auto, :color (hsl 0 0 60)})}
-      (code {:innerHTML (string/trim (cirru-edn/write configs {:inline? false}))})))
-    (:ui version-plugin))))
+       (let [content (string/join " " (:modules configs))]
+         (if (string/blank? content) "-" content))
+       style-value)))
+    (pre
+     {:style (merge {:max-width "100%", :overflow :auto, :color (hsl 0 0 60)})}
+     (code {:innerHTML (string/trim (cirru-edn/write configs {:inline? false}))}))
+    (:ui version-plugin)
+    (:ui modules-plugin))))
